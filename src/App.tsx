18f2fe8f2dc35ac58980256a401e5d5f6482c910
Ivan Shirykhalov <ivanshirykhalov@gmail.com>
2023-01-22 14:11:43 +0300

    set.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,33 +10,29 @@ function App() {
     const [value, setValue] = useState<number>(minValue)
 
 
-    const onChangeMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
-        setMaxValue(Number(e.currentTarget.value))
-    }
+    /*    const onChangeMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
+            setMaxValue(Number(e.currentTarget.value))
+        }
 
-    const onChangeMinValue = (e: ChangeEvent<HTMLInputElement>) => {
-        setMinValue(Number(e.currentTarget.value))
-    }
+        const onChangeMinValue = (e: ChangeEvent<HTMLInputElement>) => {
+            setMinValue(Number(e.currentTarget.value))
+        }
 
 
+        const setLS = () => {
+            localStorage.setItem('min', JSON.stringify(minValue))
 
-    const setLS = () => {
-        localStorage.setItem('min', JSON.stringify(minValue))
-        localStorage.getItem(JSON.stringify(minValue))
 
-        localStorage.setItem('max', JSON.stringify(maxValue))
-        localStorage.getItem(JSON.stringify(maxValue))
+            localStorage.setItem('max', JSON.stringify(maxValue))
 
-    }
+
+        }*/
+
 
     return (
         <div className="App">
-            <input type='' value={minValue} onChange={onChangeMinValue}/>
-            <input type='' value={maxValue} onChange={onChangeMaxValue}/>
-
-            <Counter value={value} setValue={setValue} maxValue={maxValue} minValue={minValue}/>
-            <button onClick={setLS}>set</button>
-
+            <Counter value={value} setValue={setValue} maxValue={maxValue} minValue={minValue} setMinValue={setMinValue}
+                     setMaxValue={setMaxValue}/>
         </div>
     );
 }
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Inc} from "./Inc";
 import {Reset} from "./Reset";
 import s from './Counter.module.css'
+import {Set} from "./Set";
 
 
 type CounterPropsType = {
@@ -9,15 +10,18 @@ type CounterPropsType = {
     value: number
     minValue: number
     maxValue: number
+    setMaxValue: (e: number) => void
+    setMinValue: (e: number) => void
 }
 export type ButtonPropsType = {
     title: string
     setValue: () => void
     value: number
-    maxValue?: number
-    minValue?: number
+    maxValue: number
+    minValue: number
+
 }
-export const Counter = ({setValue, value, minValue, maxValue}: CounterPropsType) => {
+export const Counter = ({setValue, value, minValue, maxValue, setMinValue, setMaxValue}: CounterPropsType) => {
 
 
     return (
@@ -26,8 +30,11 @@ export const Counter = ({setValue, value, minValue, maxValue}: CounterPropsType)
                 {value === maxValue ? <h1 className={s.max}>{value}</h1> : <h1>{value}</h1>}
             </div>
             <div className={s.button}>
-                <Inc value={value} maxValue={maxValue} setValue={() => setValue(value + 1)} title={'Inc'}/>
-                <Reset title={'Reset'} setValue={() => setValue(minValue)} value={value} minValue={minValue}/>
+                <Inc value={value} maxValue={maxValue} setValue={() => setValue(value + 1)} title={'Inc'}
+                     minValue={minValue}/>
+                <Reset title={'Reset'} setValue={() => setValue(minValue)} value={value} minValue={minValue}
+                       maxValue={maxValue}/>
+                <Set minValue={minValue} maxValue={maxValue} setMaxValue={setMaxValue} setMinValue={setMinValue}/>
             </div>
         </div>
     );
